4314fcfb4e6f5d85ed54bd2926c1fc6351ba26ea
Joe Lin <joelin000@gmail.com>
2017-11-08 15:07:01 +0800

    Add strategy code import structure.

---
+++ lib/strategy.py
@@ -0,0 +1,3 @@
+from strgy import pattern_strategy
+
+PatternStrategy = pattern_strategy.PatternStrategy
---
+++ test/pattern_strategy_test.py
@@ -0,0 +1,6 @@
+from setup import setup, run
+setup()
+
+from strategy import PatternStrategy
+
+strgy = PatternStrategy()
