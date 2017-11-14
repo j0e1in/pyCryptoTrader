583ded013df721d2135d0f8f2726f0659626aec7
Joe Lin <joelin000@gmail.com>
2017-11-14 17:51:08 +0800

    Add a basic backtrader template.

--- lib/mongo.py
+++ lib/mongo.py
@@ -1,6 +1,6 @@
 import motor.motor_asyncio as motor
 import logging
-import pandas
+import pandas as pd
 
 from utils import ld_to_dl, INF
 
@@ -21,12 +21,30 @@ class Mongo():
         await self._dump_to_file(db, collection, path, 'csv')
 
     async def _dump_to_file(self, db, collection, path, format):
-        coll = self.client.get_database(db).get_collection(collection)
-        docs = await coll.find({}, {'_id': 0}).to_list(length=INF)
-        dl = ld_to_dl(docs)
-        df = pandas.DataFrame(data=dl)
+        df = await self.read_to_dataframe(db, collection)
         if format == 'csv':
-            df.to_csv(path)
+            df.to_csv(path, index=False)
+
+    async def read_to_dataframe(self, db, collection, condition={}, *,
+                                date_parser=None,
+                                date_col=None,
+                                index_col=None,
+                                df_options={}):
+        coll = self.client.get_database(db).get_collection(collection)
+        docs = await coll.find(condition, {'_id': 0}).to_list(length=INF)
+        df = pd.DataFrame(data=docs, **df_options)
+
+        if date_parser and date_col:
+            df[date_col] = df[date_col].apply(date_parser)
+        elif date_parser and not date_col:
+            raise ValueError('date_parser is provided but date_col is not.')
+        elif date_col and not date_parser:
+            raise ValueError('date_col is provided but date_parser is not.')
+
+        if index_col:
+            df = df.set_index(index_col)
+
+        return df
 
     @staticmethod
     async def check_colums(collection, colums):
