04a71f6c6c8ccc50c1bc1243c7b61a8401333cc5
Joe Lin <joelin000@gmail.com>
2018-01-01 23:39:23 +0800

    Add test runner.

---
+++ test/test_runner.py
@@ -0,0 +1,37 @@
+from pprint import pprint
+from subprocess import Popen, PIPE
+import sys
+
+
+cmds = [
+    ['python', 'hist_data_test.py'],
+    ["python", "db_test.py"],
+    ["python", "trader_test.py"],
+    ["python", "backtest_test.py"],
+    ["python", "plot_test.py"],
+    ["python", "strategy_test.py"]
+]
+
+
+def main(argv):
+
+    starting_test = int(argv[0]) if len(argv) > 0 else 0
+
+    for i, cmd in enumerate(cmds):
+
+        if i >= starting_test - 1:
+            cmd_msg = ' '.join(cmd)
+            print(f"\n>>>>>>>>>> RUNNING {cmd_msg} <<<<<<<<<<\n")
+            p = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+
+            output, err = p.communicate()
+            rc = p.returncode
+            if rc is not 0:
+                err = err.decode('utf-8')
+                print("Error Message:\n")
+                print(err)
+                sys.exit(1)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
