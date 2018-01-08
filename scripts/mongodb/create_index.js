838e916571f616ab42eb1cccaf456dac9ff99c52
Joe Lin <joelin000@gmail.com>
2018-01-08 16:10:48 +0800

    Add script to remove duplicated ohlcvs.

--- scripts/mongodb/create_index.js
+++ scripts/mongodb/create_index.js
@@ -5,7 +5,22 @@ coll = db.getCollectionNames()
 // Create unique timestamp index for ohlcvs
 for (i = 0; i < coll.length; i++) {
   if(coll[i].includes('_ohlcv_')) {
-    db.getCollection(coll[i]).createIndex({'timestamp':1}, {'unique': true})
+    collection = db.getCollection(coll[i])
+
+    collection.aggregate([
+        { "$group": {
+            "_id": { "timestamp": "$timestamp" },
+            "dups": { "$push": "$_id" },
+            "count": { "$sum": 1 }
+        }},
+        { "$match": { "count": { "$gt": 1 } }}
+
+    ]).forEach(function(doc) {
+        doc.dups.shift();
+        collection.remove({ "_id": {"$in": doc.dups }});
+    })
+
+    collection.createIndex({"timestamp": 1},{unique:true})
   }
 }
 
