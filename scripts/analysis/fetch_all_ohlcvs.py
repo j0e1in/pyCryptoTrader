e64b191a1d33a6d42585aa742ab72aef20ed5619
Joe Lin <joelin000@gmail.com>
2018-02-10 08:38:05 +0800

    Update ohlcv  fetching dates.

--- scripts/analysis/fetch_all_ohlcvs.py
+++ scripts/analysis/fetch_all_ohlcvs.py
@@ -19,8 +19,8 @@ from ipdb import set_trace as trace
 logger = logging.getLogger()
 
 
-start = datetime(2017, 1, 31)
-end = datetime(2018, 2, 4)
+start = datetime(2018, 1, 30)
+end = datetime(2018, 2, 7)
 
 
 async def fetch_ohlcv_to_mongo(coll, exchange, symbol, timeframe):
