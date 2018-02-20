e227cfbc98147f9561e114cedf9d870b74c9a73e
Joe Lin <joelin000@gmail.com>
2018-02-21 03:50:21 +0800

    Add build_ohlcvs.py

---
+++ scripts/analysis/build_ohlcvs.py
@@ -0,0 +1,50 @@
+from setup import run, setup
+setup()
+
+from db import EXMongo
+import logging
+
+from analysis.hist_data import build_ohlcv
+
+logger = logging.getLogger()
+
+async def main():
+    target_tfs = [
+        '2h',
+        '4h',
+        '5h',
+        '7h',
+        '8h',
+        '9h',
+        '10h',
+        '11h',
+        '15h',
+        '18h',
+    ]
+
+    symbols = [
+        "BTC/USD",
+        "BCH/USD",
+        "ETH/USD",
+        "ETC/USD",
+        "DASH/USD",
+        "LTC/USD",
+        "NEO/USD",
+        "XMR/USD",
+        "XRP/USD",
+        "ZEC/USD",
+    ]
+
+    src_tf = '1h'
+    exchange = 'bitfinex'
+    mongo = EXMongo()
+
+    for target_tf in target_tfs:
+        for symbol in symbols:
+            logger.info(f"Building {exchange} {symbol} {target_tf} ohlcv")
+            await build_ohlcv(
+                mongo, exchange, symbol, src_tf, target_tf)
+
+
+if __name__ == '__main__':
+    run(main)
