e7542cfc242574ec07e0a874aebb747170058dab
Joe Lin <joelin000@gmail.com>
2018-03-13 04:52:31 +0800

    Add active orders/positions, ping and change_log_level APIs.

--- lib/api/api_server.py
+++ lib/api/api_server.py
@@ -1,10 +1,12 @@
 from concurrent.futures import FIRST_COMPLETED
 from sanic import Sanic
-from sanic.response import json
+from sanic import response
+from sanic.exceptions import abort
 
 import asyncio
 import argparse
 import copy
+import json
 
 from utils import dt_ms, config
 
@@ -21,14 +23,21 @@ class APIServer():
     # | KEEP_ALIVE_TIMEOUT | 5         | How long to hold a TCP connection open (sec)  |
 
     app = Sanic(__name__, log_config=None)
-    app.config.KEEP_ALIVE = config['apiserver']['keep_alive']
-    app.config.KEEP_ALIVE_TIMEOUT = config['apiserver']['keep_alive_timeout']
-    app.config.REQUEST_TIMEOUT = config['apiserver']['request_timeout']
-    app.config.RESPONSE_TIMEOUT = config['apiserver']['response_timeout']
 
-    def __init__(self, trader):
+    def __init__(self, trader, custom_config=None):
+        self._config = custom_config if custom_config else config
+        self.config = self._config['apiserver']
+
+        self.api_access = self.config['api_access']
+
         self.app.trader = trader
-        # setattr(self.app, 'trader', trader) # for app listener and router
+        self.app.server = self
+        self.app.config.KEEP_ALIVE = config['apiserver']['keep_alive']
+        self.app.config.KEEP_ALIVE_TIMEOUT = config['apiserver']['keep_alive_timeout']
+        self.app.config.REQUEST_TIMEOUT = config['apiserver']['request_timeout']
+        self.app.config.RESPONSE_TIMEOUT = config['apiserver']['response_timeout']
+
+        self.log_level = 'info'
 
     async def run(self, *args, **kwargs):
         """ Start server and provide some cli options. """
@@ -61,8 +70,8 @@ class APIServer():
     async def after_server_stop(app, loop):
         await app.trader.ex.ex.close()
 
-    @app.route('/account/info')
-    async def account_info(req):
+    @app.route('/account/info/<uid:string>/<ex:string>', methods=['GET'])
+    async def account_info(req, uid, ex):
         """ Query current exchange account's trading state.
             {
                 "balance": {
@@ -86,6 +95,12 @@ class APIServer():
                 ]
             }
         """
+        if not req.app.server.verified_access(uid, 'account_info'):
+            abort(401)
+
+        if ex != req.app.trader.ex.exname:
+            return response.json({ 'error': 'Exchange is not active.' })
+
         trader = req.app.trader
         active_markets = trader.ex.markets
         all_markets = list(trader.ex.ex.markets.keys())
@@ -108,14 +123,14 @@ class APIServer():
                 'margin': trader.ex.ex.markets[market]['info']['margin']
             })
 
-        return json({
+        return response.json({
             'balance': trader.ex.wallet,
             'active_markets': active,
             'inactive_markets': inactive,
         })
 
-    @app.route('/account/summary')
-    async def account_summary(req):
+    @app.route('/account/summary/<uid:string>/<ex:string>', methods=['GET'])
+    async def account_summary(req, uid, ex):
         """ Query summary of current trading session.
 
             Response:
@@ -146,8 +161,104 @@ class APIServer():
                 "PL_Eff": float
             }
         """
+        if not req.app.server.verified_access(uid, 'account_summary'):
+            abort(401)
+
+        if ex != req.app.trader.ex.exname:
+            return response.json({ 'error': 'Exchange is not active.' })
+
         summ = copy.deepcopy(await req.app.trader.get_summary())
         summ['start'] = dt_ms(summ['start'])
         summ['now'] = dt_ms(summ['now'])
 
-        return json(summ)
+        return response.json(summ)
+
+    @app.route('/account/active/orders/<uid:string>/<ex:string>', methods=['GET'])
+    async def active_orders(req, uid, ex):
+        """ Query active orders of an exchange.
+            {
+                "orders": [
+                    {
+                        "symbol": string
+                        "type": string
+                        "side": string
+                        "amount": float
+                        "filled": float
+                        "remaining": float
+                        "price": float
+                        "timestamp": int
+                        "margin": bool
+                    },
+                    ...
+                ]
+            }
+        """
+        if not req.app.server.verified_access(uid, 'active_orders'):
+            abort(401)
+
+        if ex != req.app.trader.ex.exname:
+            return response.json({ 'error': 'Exchange is not active.' })
+
+        orders = await req.app.trader.ex.fetch_open_orders()
+
+        for ord in orders:
+            del ord['average']
+            del ord['datetime']
+            del ord['fee']
+            del ord['id']
+            del ord['status']
+
+        return response.json({ 'orders': orders })
+
+    @app.route('/account/active/positions/<uid:string>/<ex:string>', methods=['GET'])
+    async def active_positions(req, uid, ex):
+        """ Query active orders of an exchange.
+        """
+        if not req.app.server.verified_access(uid, 'active_positions'):
+            abort(401)
+
+        if ex != req.app.trader.ex.exname:
+            return response.json({ 'error': 'Exchange is not active.' })
+
+        positions = await req.app.trader.ex.fetch_positions()
+
+        for i, pos in enumerate(positions):
+            positions[i] = {
+                'amount': abs(pos['amount']),
+                'base_price': pos['base_price'],
+                'PL': pos['pl'],
+                'side': 'buy' if pos['amount'] > 0 else 'sell',
+                'symbol': pos['symbol'],
+                'timestamp': pos['timestamp']
+            }
+
+        return response.json({ 'positions': positions })
+
+    @app.route('/ping', methods=['GET'])
+    async def ping(req):
+        return response.json({ 'ok': True })
+
+    ############################
+    ##       2FA Required     ##
+    ############################
+
+    @app.route('/notification/log_level/<uid:string>', methods=['POST'])
+    async def change_log_level(req, uid):
+        if not req.app.server.verified_access(uid, 'change_log_level'):
+            abort(401)
+
+        payload = json.loads(req.body)
+        if 'level' not in payload:
+            return response.json({ 'error': "Payload should contain field `level`"
+                                  " with one of values: info | debug | warn | error" })
+
+        req.app.server.log_level = payload['level']
+        return response.json({'ok': True})
+
+    def verified_access(self, uid, func):
+        if uid in self.api_access \
+        and ("all" in self.api_access[uid] \
+        or    func in self.api_access[uid]):
+            return True
+        else:
+            return False
