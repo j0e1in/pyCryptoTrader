d29ee7e0f5b5ce588abb2857b825ffaf61b20940
Joe Lin <joelin000@gmail.com>
2018-03-18 02:04:39 +0800

    Add install docker-compose commands.

--- scripts/system/dev_server_setup.sh
+++ scripts/system/dev_server_setup.sh
@@ -34,7 +34,12 @@ sudo add-apt-repository \
    stable"
 
 sudo apt-get update
-sudo apt-get install -y docker-ce docker-compose
+sudo apt-get install -y docker-ce
+
+# Install newest version of docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo rm -rf /usr/bin/docker-compose
 
 # Giving non-root access ( reboot to take effect)
 sudo usermod -aG docker $USERNAME
