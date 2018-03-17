3a5956491846343075005cbe2b0920dc518d7a51
Joe Lin <joelin000@gmail.com>
2018-03-17 19:28:18 +0800

    Update build_image.sh

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -7,39 +7,39 @@ if [ -z "$*" ]; then
 fi
 
 if [ $target = "all" ]; then
-  docker build -t pycryptotrader ../
-  docker tag pycryptotrader gcr.io/docker-reghub/pyct
+  docker build -t pyct ../
+  docker tag pyct gcr.io/docker-reghub/pyct
 
-  docker build -t pycryptotrader.trade ./trade
-  docker build -t pycryptotrader.optimize ./optimize
-  docker build -t pycryptotrader.fetch_ohlcv ./fetch_ohlcv
-  docker build -t pycryptotrader.fetch_trades ./fetch_trades
-  docker build -t pycryptotrader.build_ohlcvs ./build_ohlcvs
+  docker build -t pyct_trade ./trade
+  docker build -t pyct_optimize ./optimize
+  docker build -t pyct_fetch_ohlcv ./fetch_ohlcv
+  docker build -t pyct_fetch_trades ./fetch_trades
+  docker build -t pyct_build_ohlcvs ./build_ohlcvs
 fi
 
 if [ $target = "main" ]; then
-  docker build -t pycryptotrader ../
-  docker tag pycryptotrader gcr.io/docker-reghub/pyct
+  docker build -t pyct ../
+  docker tag pyct gcr.io/docker-reghub/pyct
 fi
 
 if [ $target = "trade" ]; then
-  docker build -t pycryptotrader.trade ./trade
+  docker build -t pyct_trade ./trade
 fi
 
 if [ $target = "optimize" ]; then
-  docker build -t pycryptotrader.optimize ./optimize
+  docker build -t pyct_optimize ./optimize
 fi
 
 if [ $target = "fetch_ohlcv" ]; then
-  docker build -t pycryptotrader.fetch_ohlcv ./fetch_ohlcv
+  docker build -t pyct_fetch_ohlcv ./fetch_ohlcv
 fi
 
 if [ $target = "fetch_trades" ]; then
-  docker build -t pycryptotrader.fetch_trades ./fetch_trades
+  docker build -t pyct_fetch_trades ./fetch_trades
 fi
 
 if [ $target = "build_ohlcvs" ]; then
-  docker build -t pycryptotrader.build_ohlcvs ./build_ohlcvs
+  docker build -t pyct_build_ohlcvs ./build_ohlcvs
 fi
 
 
@@ -49,6 +49,6 @@ fi
 # if [ -z "$IMG_TYPE" ] || [ -z "$IMG_VERSION" ]; then
 #   echo "Usage: ./build.sh [jessie | slim | alpine] [version]"
 # else
-#   docker build -t pycryptotrader:$IMG_VERSION-$IMG_TYPE -f Dockerfile-$IMG_TYPE ..
+#   docker build -t pyct:$IMG_VERSION-$IMG_TYPE -f Dockerfile-$IMG_TYPE ..
 # fi
 
