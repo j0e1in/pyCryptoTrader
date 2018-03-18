4c0e2cef10cf256942ddf50a01840d730986b35f
Joe Lin <joelin000@gmail.com>
2018-03-19 01:27:16 +0800

    Update remote_deploy_docker_stack.sh

--- scripts/system/remote_deploy_docker_stack.sh
+++ scripts/system/remote_deploy_docker_stack.sh
@@ -1,31 +1,39 @@
 #!/bin/bash
 
+TYPE=$1
+
 PROJ_DIR=pyCryptoTrader
 CUR_DIR=$(pwd)
+
+USERNAME=j0e1in
+IP=crypto.csie.io
 
 if [[ "$CUR_DIR" != */$PROJ_DIR ]]; then
   echo "ERROR: Please run this script at project root."
   exit 1
 fi
 
-cd ../
+if [ -z $TYPE ]; then
+  TYPE=test
+fi
 
-USERNAME=j0e1in
-IP=crypto.csie.io
+echo "Deploying docker stack to $USERNAME@$IP..."
+
+cd ../
 
 # Zip and upload source code
 rm -rf $PROJ_DIR.zip
-zip -9 -r --exclude=*.git* $PROJ_DIR.zip $PROJ_DIR
+zip -9 -qr --exclude=*.git* $PROJ_DIR.zip $PROJ_DIR
 scp $PROJ_DIR.zip $USERNAME@$IP:~/
 ssh $USERNAME@$IP "PROJ_DIR=pyCryptoTrader && \
                    rm -rf $PROJ_DIR && \
-                   unzip $PROJ_DIR.zip && \
+                   unzip -q $PROJ_DIR.zip && \
                    cd $PROJ_DIR && \
                    docker-compose build && \
                    docker stack rm crypto && \
                    echo \"wait for 20 seconds...\" && \
                    sleep 20 && \
-                   docker stack deploy -c docker-compose-production.yml crypto && \
+                   docker stack deploy -c docker-compose-$TYPE.yml crypto && \
                    echo \"wait for 10 seconds...\" && \
                    sleep 10 && \
                    docker service logs -f crypto_trade"
