1c6c2608bf77b0156668c6d13aac05be64425712
Joe Lin <joelin000@gmail.com>
2018-03-24 16:26:46 +0800

    Add --rate-limit option to ohlcv_stream.py

--- scripts/trading/ohlcv_stream.py
+++ scripts/trading/ohlcv_stream.py
@@ -8,12 +8,13 @@ import logging
 from api import APIServer
 from db import EXMongo
 from trading.trader import SingleEXTrader
-
+from utils import config
 
 def parse_args():
     import argparse
 
     parser = argparse.ArgumentParser()
+    parser.add_argument('--rate-limit', type=int, help="Fetch ohlcv request rate (in ms)")
     parser.add_argument('--mongo-host', type=str, help="Specify mongodb host,\n"
                                                        "eg. localhost (host connect to mongo on host)\n"
                                                        "    mongo (container connect to mongo container)\n"
@@ -26,10 +27,14 @@ def parse_args():
 async def main():
     argv = parse_args()
 
+    config['ccxt']['rate_limit'] = \
+        argv.rate_limit if argv.rate_limit else 4000
+
     mongo_host = argv.mongo_host if argv.mongo_host else None
     mongo = EXMongo(host=mongo_host)
 
     trader = SingleEXTrader(mongo, 'bitfinex', 'pattern',
+            custom_config=config,
             disable_trading=True, log=True)
 
     await trader.ex._start_ohlcv_stream()
